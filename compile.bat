f47a1712d9edca6b883b5691e7ebdaceece7349d
Jason Hatfield <104407084+SPredinova@users.noreply.github.com>
2026-04-20 19:26:02 -0500

    changed compile

--- compile.bat
+++
@@ -1,3 +0,0 @@
-mvn clean package
-cd target
-move UGG-1.0-SNAPSHOT.jar ..
---
+++ compile.ps1
@@ -0,0 +1,3 @@
+mvn clean package
+
+Move-Item -Path ".\target\UGG-1.0-SNAPSHOT.jar" -Destination ".\UGG-1.0-SNAPSHOT.jar" -Force
